1eae8a0a8943ef639d402f6c544e69f9bfc28414
石井 伸弥 <ishii.shinya@fcs.ed.jp>
2026-02-12 00:30:19 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// GitHub Pages用: リポジトリ名に合わせてbaseを設定
-// リポジトリ名が異なる場合は '/リポジトリ名/' に変更してください
+// GitHub Pages: https://shin-phys.github.io/harmonic_motion/
 export default defineConfig({
   plugins: [react()],
-  base: '/shm-simulator/',
+  base: '/harmonic_motion/',
 })
